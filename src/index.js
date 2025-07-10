dd163d18aa434943ec79626a7b3ff186619d7f3e
dadang6842 <dadang6842@gmail.com>
2025-07-10 23:09:00 +0900

    chore : swagger 설정

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
-import express from 'express'          
+import express from "express";
+import swaggerAutogen from "swagger-autogen";
+import swaggerUiExpress from "swagger-ui-express";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World! ')
-})
+// swagger 설정
+app.use(
+    "/docs",
+    swaggerUiExpress.serve,
+    swaggerUiExpress.setup(
+        {},
+        {
+            swaggerOptions: {
+                url: "/openapi.json",
+            },
+        }
+    )
+);
+
+app.get("/openapi.json", async (req, res, next) => {
+    // #swagger.ignore = true
+    const options = {
+        openapi: "3.0.0",
+        disableLogs: true,
+        writeOutputFile: false,
+    };
+    const outputFile = "/dev/null"; // 파일 출력은 사용하지 않습니다.
+    const routes = ["./src/index.js"];
+    const doc = {
+        info: {
+            title: "Bandnol",
+            description: "Bandnol 프로젝트의 API 명세입니다.",
+        },
+        host: "localhost:3000",
+    };
+
+    const result = await swaggerAutogen(options)(outputFile, routes, doc);
+    res.json(result ? result.data : null);
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello World! ");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
