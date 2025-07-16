3e6bac821d1bdd2a628d6e6e8b0fdf4a9a13288a
dadang6842 <dadang6842@gmail.com>
2025-07-16 15:30:03 +0900

    refactor : 수신/발신 곡 조회 API 분리

--- src/components/components.js
+++ src/components/components.js
@@ -1,22 +1,41 @@
-export default {
-  responses: {
+export const responses = {
     Success: {
-      description: "성공 응답",
-      content: {
-        "application/json": {
-          schema: {
-            type: "object",
-            properties: {
-              success: { type: "boolean"},
-              data: { type: "object" },
-              error: {
-                type: ["object", "null"],
-                example: null
-              }
-            }
-          },
+        description: "성공 응답",
+        content: {
+            "application/json": {
+                schema: {
+                    type: "object",
+                    properties: {
+                        success: { type: "boolean", example: true },
+                        data: { type: "object" },
+                        error: {
+                            type: ["object", "null"],
+                            example: null,
+                        },
+                    },
+                },
+            },
         },
-      },
-    },  
-  }
-}
+    },
+    Failed: {
+        description: "실패 응답",
+        content: {
+            "application/json": {
+                schema: {
+                    type: "object",
+                    properties: {
+                        success: { type: "boolean", example: false },
+                        data: { type: "object" },
+                        error: {
+                            type: ["object", "null"],
+                            example: {
+                                code: "R1301",
+                                message: "에러 메시지",
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    },
+};
