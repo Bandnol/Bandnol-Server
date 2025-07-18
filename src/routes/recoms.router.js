8a87e8d1dd929389a2bd64145eec0dfec0653751
dadang6842 <dadang6842@gmail.com>
2025-07-18 19:48:03 +0900

    fix : router 경로 수정

--- src/routes/recoms.router.js
+++ src/routes/recoms.router.js
@@ -13,7 +13,7 @@ const router = Router();
 router.get("/recoms/search/song", handleAllTracks);
 router.get("/:recomsId/sent", authenticateAccessToken, handleSentRecomsSong);
 router.get("/:recomsId/received", authenticateAccessToken, handleReceivedRecomsSong);
-router.get("/recoms/search/record", authenticateAccessToken, searchRecomSong);
-router.get("/recoms/:recomsId/comments", authenticateAccessToken, handleViewComments);
+router.get("/search/record", authenticateAccessToken, searchRecomSong);
+router.get("/:recomsId/comments", authenticateAccessToken, handleViewComments);
 
 export default router;
