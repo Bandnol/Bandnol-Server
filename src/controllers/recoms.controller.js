08146696ba017ae41e24f025c707e244fdacee90
kny2383 <kny2383@gmail.com>
2025-07-20 19:13:30 +0900

    feat : 추천 기록 리스트 조회 API 구현

--- src/controllers/recoms.controller.js
+++ src/controllers/recoms.controller.js
@@ -9,6 +9,7 @@ import {
     viewReplies,
     calendarRecomsSong,
     sendReplies,
+    listRecomsSong,
 } from "../services/recoms.service.js";
 import { searchSpotifyTracks } from "../services/spotify.service.js";
 import { NotFoundKeywordError } from "../errors.js";
@@ -327,6 +328,7 @@ export const handleCalendarRecomSong = async (req, res, next) => {
     } catch(err) {
       next(err);
     }  
+};
 
 export const handleSendReplies = async (req, res, next) => {
     /*
@@ -363,4 +365,13 @@ export const handleSendReplies = async (req, res, next) => {
     } catch (err) {
         next(err);
     }
+};
+
+export const handleListRecomSong = async (req, res, next) => {
+    try {
+      const listRecomsData = await listRecomsSong(req.user.id);
+      res.status(StatusCodes.OK).success(listRecomsData);
+    } catch(err) {
+      next(err);
+    }  
 };
