98eeff43f418f2ac27204948eae5ff3be3cbff86
Kim Nayoung <80757985+kny2383@users.noreply.github.com>
2025-07-20 09:37:23 +0900

    fix : 라우터 경로 수정

--- src/routes/recoms.router.js
+++ src/routes/recoms.router.js
@@ -17,7 +17,7 @@ const router = Router();
 router.get("/search/song", handleAllTracks);
 router.get("/:recomsId/sent", authenticateAccessToken, handleSentRecomsSong);
 router.get("/:recomsId/received", authenticateAccessToken, handleReceivedRecomsSong);
-router.get("/recoms/search/record", authenticateAccessToken, handleSearchRecomSong);
+router.get("/search/record", authenticateAccessToken, handleSearchRecomSong);
 router.post("/",authenticateAccessToken, handleAddRecoms);
 router.get("/:recomsId/comments", authenticateAccessToken, handleViewComments);
 router.patch("/:recomsId/likes", authenticateAccessToken, handleModifyLikeStatus);
