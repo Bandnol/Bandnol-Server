cf5fd279a3fa7667274aec99ab16d922947ecda1
dadang6842 <dadang6842@gmail.com>
2025-07-30 22:11:58 +0900

    fix : 추천 아티스트 목록 조회 에러 해결

--- src/cron/scheduler.js
+++ src/cron/scheduler.js
@@ -13,11 +13,11 @@ export const songScheduler = async () => {
     cron.schedule("* * * * *", async () => {
         try {
             let min = new Date().getMinutes();
-            console.log(`running a task every minute: ${min}`);
+            //console.log(`running a task every minute: ${min}`);
             const recomsList = await getRecomsWithNoReceiver();
-            console.log("recomsList: ", recomsList);
+            //console.log("recomsList: ", recomsList);
             const userList = await getUserList();
-            console.log("userList: ", userList);
+            //console.log("userList: ", userList);
             let i = 0;
 
             for (let user of userList) {
@@ -25,34 +25,34 @@ export const songScheduler = async () => {
 
                 // 노래를 보내지 않았을 경우
                 if (!isSent) {
-                    console.log("노래를 추천하지 않았습니다: ", user.id);
+                    //console.log("노래를 추천하지 않았습니다: ", user.id);
                     continue;
                 }
 
                 if (!recomsList[i]) {
                     // 보낼 추천 곡이 없는 경우 - AI 생성
-                    console.log("AI로 노래를 생성합니다.");
+                    //console.log("AI로 노래를 생성합니다.");
                     await sendAIRecoms(user.id);
-                    console.log("AI 노래 생성 완료!");
+                    //console.log("AI 노래 생성 완료!");
                 } else if (user.id === recomsList.senderId) {
                     // 본인이 보낸 추천 곡을 추천 받았을 경우
-                    console.log("본인이 보낸 추천 곡을 추천 받았습니다.");
+                    //console.log("본인이 보낸 추천 곡을 추천 받았습니다.");
                     i++;
                     if (!recomsList[i]) {
-                        console.log("다음 리스트가 없습니다. AI로 노래를 생성합니다.");
+                        //console.log("다음 리스트가 없습니다. AI로 노래를 생성합니다.");
                         await sendAIRecoms(user.id);
-                        console.log("AI 노래 생성 완료!");
+                        //console.log("AI 노래 생성 완료!");
                     } else {
-                        console.log(`다음 리스트가 있습니다. ${i}번째: `, recomsList[i]);
+                        //console.log(`다음 리스트가 있습니다. ${i}번째: `, recomsList[i]);
                         await sendUserRecoms();
-                        console.log("receiver 등록 완료!: ", user.id);
+                        //console.log("receiver 등록 완료!: ", user.id);
                         i++;
                     }
                 } else {
                     // 보낼 추천 곡이 있고 sender가 본인이 아닌 경우
-                    console.log(`다음 리스트가 있습니다. ${i}번째: `, recomsList[i]);
+                    //console.log(`다음 리스트가 있습니다. ${i}번째: `, recomsList[i]);
                     await sendUserRecoms(recomsList[i].id, user.id);
-                    console.log("receiver 등록 완료!");
+                    //console.log("receiver 등록 완료!");
                     i++;
                 }
 
