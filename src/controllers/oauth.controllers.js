f071dba00da45a687e4d924b089d389ed9786764
wldsmdl7 <wldmsdl5835@gmail.com>
2025-08-01 10:40:02 +0900

    feat : 리프레시 토큰 발급 로직 추가

--- src/controllers/oauth.controllers.js
+++ src/controllers/oauth.controllers.js
@@ -1,16 +1,23 @@
-import { AuthorizationCodeError, TokenError } from "../errors.js";
-import { generateToken } from "../utils/token.js";
+import { AuthTokenError, TokenError } from "../errors.js";
+import { generateRefreshToken, generateToken } from "../utils/token.js";
 import { getKakaoUser } from "../configs/auth.config.js"
-import { findOrCreateUser } from "../repositories/users.repository.js"
+import { findOrCreateUser, findUserByToken } from "../repositories/users.repository.js"
+import { updateUserRefreshToken } from "../repositories/users.repository.js";
 import pkg from '@prisma/client';
 import { StatusCodes } from "http-status-codes";
 const { SocialType } = pkg;
+import jwt from 'jsonwebtoken';
+
 
 export const handleKakaoLogin = async (req, res, next) => {
     /*
     #swagger.summary = '카카오톡 소셜 로그인 API';
     #swagger.responses[200] = {
         $ref: "#/components/responses/Success"
+    };
+
+    #swagger.responses[401] = {
+      $ref: "#/components/responses/TokenError"
     };
     */
    try{
@@ -28,10 +35,48 @@ export const handleKakaoLogin = async (req, res, next) => {
     const user = await findOrCreateUser(name, email, SocialType.KAKAO);
 
     const token = generateToken({id: user.id});
+    const refreshToken = generateRefreshToken({id : user.id})
 
-    res.status(StatusCodes.OK).success({token, user});
+    await updateUserRefreshToken(user.id, refreshToken);
+
+    res.status(StatusCodes.OK).success({token,refreshToken, user});
    } catch (err) {
     console.error("Kakao 로그인 실패", err);
-    res.status(500).json({ message: "Kakao 로그인 실패" });
+    res.status(500).json({ code: 500, message: `카카오 로그인 실패 ${err}` });
   }
 }
+
+export const handleRefreshAccessToken = async (req, res) => {
+   /*
+    #swagger.summary = 'AccessToken 재발급 API';
+    #swagger.responses[200] = {
+        $ref: "#/components/responses/Success"
+    };
+
+    #swagger.responses[401] = {
+      $ref: "#/components/responses/TokenError"
+    };
+
+    #swagger.responses[403] = {
+      $ref: "#/components/responses/AuthError"
+    };
+    */
+
+  const { refreshToken } = req.body;
+  if (!refreshToken) throw new TokenError("리프레시 토큰이 전달되지 않았습니다");
+
+  try {
+    const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
+    console.log(decoded);
+    const user = await findUserByToken(decoded.id);
+
+    if (!user || user.refreshToken !== refreshToken) {
+      throw new AuthTokenError("유효하지 않은 토큰입니다");
+    }
+
+    const newAccessToken = generateToken({ id: user.id });
+    res.status(200).json({ token: newAccessToken });
+  } catch (err) {
+    res.status(500).json({ code: 500, message: `AccessToken 재발급 실패 ${err}` });
+  }
+};
--- src/routes/oauth.router.js
+++ src/routes/oauth.router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { handleKakaoLogin } from "../controllers/oauth.controllers.js"
+import { handleKakaoLogin, handleRefreshAccessToken } from "../controllers/oauth.controllers.js"
 
 const router = Router();
 
 router.post("/callback/kakao", handleKakaoLogin);
+router.patch("/refresh/token", handleRefreshAccessToken);
 
 export default router;
